b8adf2f17b76a12c5ccff347920c545b8ee83f43
phreno <losgoomy@gmail.com>
2020-03-01 07:40:01 +0100

    fix #1 #2 correction du routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageInconnueComponent } from './page-inconnue/page-inconnue.component';
 
 const appRoutes: Routes = [
-  { path: 'action', component: ArticleComponent },
-  { path: 'page-inconnue', component: PageInconnueComponent }
+  { path: 'article', component: ArticleComponent },
+  { path: '**', component: PageInconnueComponent }
 ];
 
 @NgModule({
